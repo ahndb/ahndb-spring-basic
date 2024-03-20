19551f653797514fadcf92196bf5fc7c8cf5b1eb
ahndb <asi0000@nate.com>
2024-03-20 16:51:13 +0900

    240320 16:50 Update_sprign_basic

--- src/main/java/com/sanghyun/basic/controller/RequestDataController.java
+++ src/main/java/com/sanghyun/basic/controller/RequestDataController.java
@@ -4,9 +4,13 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.sanghyun.basic.dto.SampleDto;
 
 @RestController
 // HTTP *localhost:4000/request-data-/**
@@ -30,7 +34,10 @@ public class RequestDataController {
   // - 모든 HTTP 메서드에서 URL의 특정 패턴에 따라서 데이터를 추출하는 방식
 
   // HTTP DELETE localhost:4000/request-data/path-variable
-  @DeleteMapping("/path-variable/{age}/{name}")
+  
+  @DeleteMapping({
+    "/path-variable/{age}/{name}",
+    "/path-variable/{age}"})
   // http://localhost:4000/request-data/path-variable/10
   public String deletePathVariable(
       @PathVariable("age") Integer age,
@@ -61,4 +68,15 @@ public class RequestDataController {
     return "getPathVariable2";
   }
 
+  // @RequestBody() :
+  //  - POST, PATCH, PUT 처럼 RequestBody로 데이터를 전송하는 메서드에서 데이터를 가져오기 위해 사용
+  
+  // HTTP POST localhost:4000/request-data/post
+  @PostMapping("/post")
+  public String post(
+    // @RequestBody String text
+    @RequestBody SampleDto dto
+  ){
+    return "전송한 데이터 : " + dto.toString();
+  }
 }
