fd90940c90219c5a865eb0cb3fd39483f3dc8da9
ahndb <asi0000@nate.com>
2024-03-20 14:55:23 +0900

    240320 Update_requestDataController

--- src/main/java/com/sanghyun/basic/controller/BasicController.java
+++ src/main/java/com/sanghyun/basic/controller/BasicController.java
@@ -20,8 +20,10 @@ import org.springframework.web.bind.annotation.PutMapping;
 // @RestController = @Controller + @ResponseBody
 @RestController
 // @RequestMapping() : HTTP 요청에 클래스와 메서드를 맵핑하기 위한 어노테이션
-// @RequestMapping(value = "/main", method = {RequestMethod.GET})
 // HTTP GET localhost:4000/main/** 
+// @RequestMapping(value = "/main", method = {RequestMethod.GET})
+
+// HTTP localhost:4000/main/**
 @RequestMapping("/main")
 public class BasicController {
   
