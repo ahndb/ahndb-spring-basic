139ae001c38527a5bc691b0db70f6d3ba4b15c5b
ahndb <asi0000@nate.com>
2024-03-25 14:51:10 +0900

    240325 14:50 Create service folder

---
+++ src/main/java/com/sanghyun/basic/service/BasicService.java
@@ -0,0 +1,14 @@
+package com.sanghyun.basic.service;
+
+public interface BasicService {
+  // interfacce의 메서드는 반드시 public abstract이어야 함
+  // public abstract String getHello();
+
+  // public abstract 제어자는 생략해도 됨
+  String getHello();
+}
+
+
+
+
+
---
+++ src/main/java/com/sanghyun/basic/service/implement/BasicServiceImplement.java
@@ -0,0 +1,11 @@
+package com.sanghyun.basic.service.implement;
+
+import com.sanghyun.basic.service.BasicService;
+
+public class BasicServiceImplement implements BasicService {
+
+  @Override
+  public String getHello() {
+    return "Hello Springboot!!";
+  }
+}
