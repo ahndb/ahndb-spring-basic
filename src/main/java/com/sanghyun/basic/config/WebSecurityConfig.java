390a75800b334ba49a9dea53c4b67d6b9992e814
ahndb <asi0000@nate.com>
2024-04-16 15:51:56 +0900

    240416 15:50 Update FailauthenticateEnterPoint

--- src/main/java/com/sanghyun/basic/config/WebSecurityConfig.java
+++ src/main/java/com/sanghyun/basic/config/WebSecurityConfig.java
@@ -1,5 +1,7 @@
 package com.sanghyun.basic.config;
 
+import java.io.IOException;
+
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,6 +11,8 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
@@ -17,6 +21,9 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.sanghyun.basic.filter.JwtAuthenticationFilter;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 
 // Configuration : Spring boot 기본설정 지정 및 변경
@@ -87,7 +94,10 @@ public class WebSecurityConfig {
             .requestMatchers("/student", "/student/**").hasRole("STUDENT") // 2.
             // 인증된 사용자는 모두 접근을 허용함
             .anyRequest().authenticated() // 3.
-        );
+        )
+        //# 인증 과정 중에 발생한 예외 처리
+        .exceptionHandling(exceptionHandling -> exceptionHandling
+          .authenticationEntryPoint(new FailedAuthenticationEntryPoint()));
 
     // # 우리가 생성한 jwtAuthenticationFilter를 UsernamePasswordAuthenticationFilter 이전에 등록
     security.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
@@ -111,3 +121,20 @@ public class WebSecurityConfig {
   }
 
 }
+
+// 인증 실패 처리를 위한 커스텀 예외 처리(AuthenticationEntryPoint 인터페이스 구현)
+class FailedAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+  @Override
+  public void commence(HttpServletRequest request, HttpServletResponse response, 
+    AuthenticationException authException) throws IOException, ServletException {
+    
+    authException.printStackTrace();
+
+    response.setContentType("application/json;charset=UTF-8");
+    response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+    response.getWriter().write("{ \"message\":\"인증에 실패했습니다.\" }");
+
+  }
+
+} 
