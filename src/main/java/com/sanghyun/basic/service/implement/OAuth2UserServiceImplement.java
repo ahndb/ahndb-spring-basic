57e461d8f40439e59f84670358cae9fe69e36e24
ahndb <asi0000@nate.com>
2024-04-18 15:57:06 +0900

    240418 15:50 Update key

--- src/main/java/com/sanghyun/basic/service/implement/OAuth2UserServiceImplement.java
+++ src/main/java/com/sanghyun/basic/service/implement/OAuth2UserServiceImplement.java
@@ -12,14 +12,14 @@ import com.sanghyun.basic.entity.CustomOAuthUser;
 // OAuth2.0 인증 결과를 비즈니스 로직에 사용하는 서비스
 // DefaultOAuth2UserService를 확장하여 작성
 public class OAuth2UserServiceImplement extends DefaultOAuth2UserService {
-  
 
   // loadUser 메서드 :
   // - OAuth2.0 인증 성공 시에 인증 서버로부터 받은 request 객체를 사용하여 해당 서비스에 클라이언트의 정보를 로드하는 작업
   @Override
-	public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
-    
+  public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+
     String oauthClientName = userRequest.getClientRegistration().getClientName();
+    System.out.println(oauthClientName);
     OAuth2User oAuth2User = super.loadUser(userRequest);
 
     String name = (String) oAuth2User.getAttributes().get("login");
@@ -27,4 +27,3 @@ public class OAuth2UserServiceImplement extends DefaultOAuth2UserService {
     return new CustomOAuthUser(name, oAuth2User.getAttributes(), oAuth2User.getAuthorities());
   }
 }
-
